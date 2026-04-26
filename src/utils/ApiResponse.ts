951c6cc420aa1f3868068d0414c430e1fe51e6fb
pietenium <pietech1618@protonmail.com>
2026-04-26 04:05:05 -0400

    // update AI response

---
+++ src/utils/ApiResponse.ts
@@ -0,0 +1,23 @@
+import { Response } from "express";
+
+export class ApiResponse<T> {
+  public readonly success: true = true;
+  public readonly statusCode: number;
+  public readonly message: string;
+  public readonly data: T;
+
+  constructor(statusCode: number, message: string, data: T) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+  }
+
+  public send(res: Response): void {
+    res.status(this.statusCode).json({
+      success: this.success,
+      statusCode: this.statusCode,
+      message: this.message,
+      data: this.data,
+    });
+  }
+}
