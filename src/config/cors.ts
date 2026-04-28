cb682a0da79a3d93d9335f12dc5b71c0766a7826
pietenium <pietech1618@protonmail.com>
2026-04-27 21:03:32 -0400

    setup nodemailer.ts, cors.ts

---
+++ src/config/cors.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import chalk from "chalk";
+import { env } from "./env";
+
+const allowedOrigins = [env.CLIENT_PUBLIC_URL, env.CLIENT_DASHBOARD_URL];
+
+export const corsOptions: cors.CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log(chalk.yellow(`CORS blocked origin: ${origin}`));
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
