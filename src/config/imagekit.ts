7fe78b0ef72ee7a2dba071cbe02c79294ad88c44
pietenium <pietech1618@protonmail.com>
2026-04-27 21:26:37 -0400

    Update Imagekit.ts

--- src/config/imagekit.ts
+++ src/config/imagekit.ts
@@ -1,8 +1,10 @@
 import ImageKit from "@imagekit/nodejs";
 import { env } from "./env";
 
-export const imagekit = new ImageKit({
+const imagekitConfig = {
   publicKey: env.IMAGEKIT_PUBLIC_KEY,
   privateKey: env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: env.IMAGEKIT_URL_ENDPOINT,
-});
+};
+
+export const imagekit = new ImageKit(imagekitConfig);
