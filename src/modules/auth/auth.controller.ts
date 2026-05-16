3c7b629daa8a10d37ed2a9dd01c84091e9739425
pietenium <pietech1618@protonmail.com>
2026-05-16 06:17:22 -0400

    CookiesOption Change

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,3 @@
-import { env } from "@config/env";
 import { AUTH_MESSAGES } from "@constants/messages.constant";
 import { ApiError } from "@utils/ApiError";
 import { ApiResponse } from "@utils/ApiResponse";
@@ -16,9 +15,8 @@ import { authService } from "./auth.service";
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: env.NODE_ENV === "production",
-  sameSite:
-    env.NODE_ENV === "production" ? ("none" as const) : ("lax" as const),
+  secure: true,
+  sameSite: "none" as const,
   maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
 };
 
